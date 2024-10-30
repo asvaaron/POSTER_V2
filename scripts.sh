88528de64e820eea27342e4963e96d9c47dea6dc
asvaaron <asvaaron@gmail,com>
2024-10-30 00:35:46 -0600

    Add scripts for training and validation
    Add training scripts
    Add validation scripts

--- scripts.sh
+++ scripts.sh
@@ -8,6 +8,6 @@ pip install thop
 pip install torchsampler
 
 # Test 
-python main.py --data data/raf-db/ --evaluate checkpoints
+python main.py --data ./data/raf-db/ --evaluate ./checkpoint/raf-db-model_best.pth
 
-python main.py --data data/raf-db/ --data_type RAF-DB --lr 3.5e-5 --batch-size 144 --epochs 200 --gpu 0
+python main.py --data ./data/raf-db/ --data_type RAF-DB --lr 3.5e-5 --batch-size 110 --epochs 200 --gpu 0
